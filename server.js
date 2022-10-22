c1ef77bd17b662112d7c6fa3c52d6c67f46ac24f
Andrew Dippel <a.t.dippel@gmail.com>
2022-10-22 15:32:04 +1100

    fix add function of roles and start add function for employee

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql2 = require('mysql2');
 const inquirer = require('inquirer');
+const { filter } = require('rxjs');
 
 const db = mysql2.createConnection(
     {
@@ -23,7 +24,7 @@ function dataChoices() {
             type: 'list',
             name: 'choice',
             message: 'please select one of the following',
-            choices: ['View All Departments', 'Add Department', 'View all roles', 'Add role']
+            choices: ['View All Departments', 'Add Department', 'View all roles', 'Add role', 'View Employees', 'Add employee']
         }
     ])
         .then(data => {
@@ -31,11 +32,15 @@ function dataChoices() {
                 case 'View All Departments':
                     return viewAllDepartments();
                 case 'Add Department':
-                    return addDepartment()
+                    return addDepartment();
                 case 'View all roles':
-                    return viewAllRole()
+                    return viewAllRole();
                 case 'Add role':
-                    return addRole()
+                    return addRole();
+                case 'View Employees':
+                    return viewEmployees();
+                case 'Add Employees':
+                    return addEmployee();
             }
         })
 
@@ -85,6 +90,8 @@ function addRole() {
     const sql = `SELECT * FROM departments`;
 
     db.query(sql, (err, rows) => {
+        const arr = rows.map(dept => dept.id);
+
         return inquirer.prompt([
             {
                 name: 'title',
@@ -101,17 +108,58 @@ function addRole() {
         ])
             .then(data => {
                 const sql = `INSERT INTO roles (title, salary, department_id)
-        VALUES (?)`;
-                const params = [data.title, data.salary, data.choice];
+        VALUES (?,?,?)`;
+                const params = [data.title, data.salary, arr];
                 db.query(sql, params, (err, rows) => {
+                    if (err) console.log(err);
                     console.log('Role added')
-                    console.table(rows)
+                    //console.table(data)
                     dataChoices();
                 });
             })
     });
+}
 
+function viewEmployees() {
+    //mysql query to get all employees
+    const sql = `SELECT * FROM employees JOIN roles ON employees.role_id = roles.id`;
+    db.query(sql, (err, rows) => {
+        console.table(rows)
+        dataChoices();
+    });
+}
 
+function addEmployee() {
+   //const sql = `SELECT * FROM roles`;
+
+    db.query(sql, (err, rows) => {
+        //const filteredArr = rows.filter(dept => dept.id)
+       // const arr = rows.map(dept => dept.id);
+
+        return inquirer.prompt([
+            {
+                name: 'first_name',
+                message: 'Employees first name?',
+            }, {
+                name: 'last_name',
+                message: 'Employees last name?',
+            }, {
+                name: 'manager',
+                message: 'Manager the employee will report to?',
+            }
+        ])
+            .then(data => {
+                const sql = `INSERT INTO employees (first_name, last_name, manager)
+        VALUES (?,?,?)`;
+                const params = [data.title, data.salary, arr];
+                db.query(sql, params, (err, rows) => {
+                    if (err) console.log(err);
+                    console.log('Role added')
+                    //console.table(data)
+                    dataChoices();
+                });
+            })
+    });
 }
 
 init()
